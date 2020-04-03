caffeda48bc432562c2f07fd87225e7817083743
makhnev-a <ray.makhnev@gmail.com>
2020-04-03 17:34:57 +0300

    add Header and Technologies component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-      <div>
-        <div>One div</div>
-        <div className="App">
-          <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-            <li>react</li>
-          </ul>
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
         </div>
-      </div>
-  );
+    );
+};
+
+const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#test'>Home</a>
+            <a href='#test1'>News feed</a>
+            <a href='#test2'>Messges</a>
+        </div>
+    );
 };
 
 export default App;
