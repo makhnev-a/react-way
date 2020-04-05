c6505185a758dd4bf48d6ef69e3e316adedfdff5
makhnev-a <ray.makhnev@gmail.com>
2020-04-05 15:27:48 +0300

    import, export components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Technologies from "./Technologies";
+import Header from "./Header";
 import './App.css';
 
 const App = () => {
@@ -6,29 +8,6 @@ const App = () => {
         <div>
             <Header/>
             <Technologies/>
-        </div>
-    );
-};
-
-const Technologies = () => {
-    return (
-        <div className="App">
-            <ul>
-                <li>html</li>
-                <li>css</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    );
-};
-
-const Header = () => {
-    return (
-        <div>
-            <a href='#test'>Home</a>
-            <a href='#test1'>News feed</a>
-            <a href='#test2'>Messges</a>
         </div>
     );
 };
