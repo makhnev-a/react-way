db718b6a4dae26eb75c28c2a782bb4813c766055
makhnev-a <ray.makhnev@gmail.com>
2020-04-05 22:37:44 +0300

    folder structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src="../logo2.jpg" alt='logo'/>
+            <img src="../../logo2.jpg" alt='logo'/>
         </header>
     );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
@@ -7,23 +8,12 @@ const Profile = () => {
             <div>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvzUlAU015rlbnoipDlT4NrMSLuLYch8TWdOqHb2McaDI7WU1P&usqp=CAU"
-                    alt='Profile img'/>
+                    alt='MyPosts img'/>
             </div>
             <div>
                 ava + description
             </div>
-            <div className={s.posts}>
-                My posts
-                <div className={s.item}>
-                    new post
-                </div>
-                <div className={s.item}>
-                    post 1
-                </div>
-                <div className={s.item}>
-                    post 2
-                </div>
-            </div>
+            <MyPosts/>
         </div>
     );
 };
