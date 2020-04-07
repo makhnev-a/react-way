c30c9edc8c5d6bc3eba854cbfc6b15a97e54fee3
makhnev-a <ray.makhnev@gmail.com>
2020-04-07 19:02:58 +0300

    add dialogs grid

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,22 @@ import style from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div>
-            dialogs
+            <div className={style.dialogs}>
+                <div className={style.dialogsItems}>
+                    <div className={style.dialog + ' ' + style.active}>Dimych</div>
+                    <div className={style.dialog}>Andrey</div>
+                    <div className={style.dialog}>Sveta</div>
+                    <div className={style.dialog}>Sasha</div>
+                    <div className={style.dialog}>Viktor</div>
+                    <div className={style.dialog}>Valera</div>
+                </div>
+
+                <div className={style.messages}>
+                    <div className={style.message}>Hello</div>
+                    <div className={style.message}>How are you?</div>
+                    <div className={style.message}>Yo</div>
+                </div>
+            </div>
         </div>
     );
 };
