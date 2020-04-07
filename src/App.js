79d0cb4429be23b394cbcf1c0411da827657f7e1
makhnev-a <ray.makhnev@gmail.com>
2020-04-07 17:59:51 +0300

    add another pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from 'react-router-dom';
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
@@ -15,8 +18,9 @@ const App = () => {
                 <div className="app-wrapper-content">
                     <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/profiles' component={Profile}/>
-                    {/*<Dialogs/>*/}
-                    {/*<Profile/>*/}
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="/profiles">Profile</a>
+                <a href={"/profiles"}>Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/dialogs">Messages</a>
+                <a href={"/dialogs"}>Messages</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a href={'/news'}>News</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <a href={'/music'}>Music</a>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <a href={'/settings'}>Settings</a>
             </div>
         </nav>
     );
