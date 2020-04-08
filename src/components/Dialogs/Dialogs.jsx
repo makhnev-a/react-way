0b2eee9c1fd611d9836739a66ca72ef061883f27
makhnev-a <ray.makhnev@gmail.com>
2020-04-08 18:09:46 +0300

    add sub routes

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import style from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div>
             <div className={style.dialogs}>
                 <div className={style.dialogsItems}>
-                    <div className={style.dialog + ' ' + style.active}>Dimych</div>
-                    <div className={style.dialog}>Andrey</div>
-                    <div className={style.dialog}>Sveta</div>
-                    <div className={style.dialog}>Sasha</div>
-                    <div className={style.dialog}>Viktor</div>
-                    <div className={style.dialog}>Valera</div>
+                    <div className={style.dialog + ' ' + style.active}>
+                        <NavLink to={'/dialogs/1'}>Dimych</NavLink>
+                    </div>
+                    <div className={style.dialog}>
+                        <NavLink to={'/dialogs/2'}>Andrey</NavLink>
+                    </div>
+                    <div className={style.dialog}>
+                        <NavLink to={'/dialogs/3'}>Sveta</NavLink>
+                    </div>
+                    <div className={style.dialog}>
+                        <NavLink to={'/dialogs/4'}>Sasha</NavLink>
+                    </div>
+                    <div className={style.dialog}>
+                        <NavLink to={'/dialogs/5'}>Viktor</NavLink>
+                    </div>
+                    <div className={style.dialog}>
+                        <NavLink to={'/dialogs/6'}>Valera</NavLink>
+                    </div>
                 </div>
 
                 <div className={style.messages}>
