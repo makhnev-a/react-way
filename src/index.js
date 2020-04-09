bc1de7117077b7772a74faa8b5a56d771e02e822
makhnev-a <ray.makhnev@gmail.com>
2020-04-09 16:40:57 +0300

    use render attr in route and set data to index.js fix

--- src/index.js
+++ src/index.js
@@ -4,11 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let dialogs = [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sveta'},
+    {id: 4, name: 'Sasha'},
+    {id: 5, name: 'Viktor'},
+    {id: 6, name: 'Valera'},
+];
+
+let messages = [
+    {id: 1, message: 'Hello'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'},
+];
+
+let posts = [
+    {likeCount: 2, message: 'Hi, how are you?'},
+    {likeCount: 10, message: 'It\'s my first post!'}
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App dialogs={dialogs} messages={messages} posts={posts}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
