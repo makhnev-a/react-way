f19bfa52e3db1eafe46737071ef1289f00562305
makhnev-a <ray.makhnev@gmail.com>
2020-04-10 21:12:53 +0300

    add onClick + ref

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,25 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    let postsItems = props.posts.map(post => <Post message={post.message} likeCount={post.likeCount}/>);
+    let postsItems = props.posts
+        .map(post => <Post message={post.message} likeCount={post.likeCount}/>);
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    };
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea>
+                <textarea ref={newPostElement}>
                 </textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={s.posts}>
                 {postsItems}
