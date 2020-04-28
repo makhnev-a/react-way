07cbe49cdcad885565fc46cde310bd90daa25d18
makhnev-a <ray.makhnev@gmail.com>
2020-04-28 11:01:32 +0300

    fix profile.reducer 2 and dialogs.reducer

--- src/redux/dialogs.reducer.js
+++ src/redux/dialogs.reducer.js
@@ -21,16 +21,21 @@ const initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
+    let stateCopy;
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageText = action.body;
-            return state;
+            stateCopy = {...state};
+            stateCopy.newMessageText = action.body;
+            return stateCopy;
         case SEND_MESSAGE:
             let body = state.newMessageText;
 
-            state.newMessageText = '';
-            state.messages.push({id: 6, message: body});
-            return state;
+            stateCopy = {
+                ...state,
+                newMessageText: '',
+                messages: [...state.messages, {id: 6, message: body}]
+            };
+            return stateCopy;
         default:
             return state;
     }
