b2e0ab0bd7030ca1af5ad7c335e23fed1d2bc090
makhnev-a <ray.makhnev@gmail.com>
2020-06-10 13:35:09 +0300

    add redirect to messages and profile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/Login/Login";
 
 const App = (props) => {
     let profilePage = () =>
@@ -36,6 +37,10 @@ const App = (props) => {
                 <Route
                     path={'/users'}
                     render={usersPage}
+                />
+                <Route
+                    path={'/login'}
+                    render={() => <LoginPage/>}
                 />
                 <Route path={'/news'} component={News}/>
                 <Route path={'/music'} component={Music}/>
