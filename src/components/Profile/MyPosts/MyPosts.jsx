eb2f4e372c4e910f37ac4529609d05866cca95ff
makhnev-a <ray.makhnev@gmail.com>
2020-07-17 17:52:12 +0300

    shouldComponentUpdate && React.memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,47 +7,35 @@ import {Textarea} from "../../common/FormControls/FormControls";
 
 window.props = [];
 
-class MyPosts extends React.Component {
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({a: 12});
-        }, 3000);
-    }
+const MyPosts = React.memo(props => {
+    console.log('Render');
+    window.props.push(props);
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return nextProps !== this.props || nextState !== this.state;
-    }
+    console.log(props);
 
-    render() {
-        console.log('Render');
-        window.props.push(this.props);
+    let newPostElement = React.createRef();
 
-        console.log(this.props);
-
-        let newPostElement = React.createRef();
-
-        let postsItems = this.props.posts
-            .map((post, index) =>
-                <Post
-                    key={`${post.message} + ${index}`}
-                    message={post.message}
-                    likeCount={post.likeCount}
-                />
-            );
-
-        let onAddPost = (values) => this.props.addPost(values.newPostText);
-
-        return (
-            <div className={s.postsBlock}>
-                <h3>My posts</h3>
-                <AddNewPostFormRedux onSubmit={onAddPost}/>
-                <div className={s.posts}>
-                    {postsItems}
-                </div>
-            </div>
+    let postsItems = props.posts
+        .map((post, index) =>
+            <Post
+                key={`${post.message} + ${index}`}
+                message={post.message}
+                likeCount={post.likeCount}
+            />
         );
-    }
-}
+
+    let onAddPost = (values) => props.addPost(values.newPostText);
+
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
+            <div className={s.posts}>
+                {postsItems}
+            </div>
+        </div>
+    );
+});
 
 const maxLength10 = maxLengthCreator(10);
 
